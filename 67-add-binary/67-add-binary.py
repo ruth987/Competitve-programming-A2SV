d083d412cb6956b7b271388b2e4be396ed6acf80
ruth987 <99682266+ruth987@users.noreply.github.com>
2022-10-08 06:20:49 -0700

    Time: 75 ms (16.43%), Space: 14 MB (25.48%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,10 +1,6 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        a_dec = int(a, 2)
-        b_dec = int(b, 2)
-        
-        sum_dec = a_dec+b_dec
-        return bin(sum_dec)[2:]
+        return bin(int(a, 2)+int(b, 2))[2:]
         
         
         
