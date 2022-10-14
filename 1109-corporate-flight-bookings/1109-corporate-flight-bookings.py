90d13628302eb6f0e78d4bb93cccfeb2694f62e3
ruth987 <99682266+ruth987@users.noreply.github.com>
2022-10-14 00:40:29 -0700

    Time: 1135 ms (76.30%), Space: 28 MB (69.81%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
@@ -0,0 +1,19 @@
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        events = []
+        for (start, end, seats) in bookings:
+            events.append((start-1, seats))
+            events.append((end+1-1, -seats))
+        events.sort()
+        results = [0]*n
+        for event in events:
+            if event[0] >= n:
+                continue
+            results[event[0]]+=event[1]
+            
+        for i in range(1, n):
+            results[i] += results[i-1]
+        return results 
+        
+                
+        
