e738a32ec7c7a96fe8a2938d24a90471b1497284
ruth987 <99682266+ruth987@users.noreply.github.com>
2022-10-16 19:52:29 -0700

    Time: 57 ms (49.00%), Space: 13.9 MB (11.66%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,13 +1,5 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        sentence = collections.Counter(sentence)
-        alphabet = ('q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m')
-        count = 0
-        
-        for letter in alphabet:
-            if letter in sentence:
-                count+=1
-                
-        if count == 26:
+        if len(set(sentence)) == 26:
             return True
         return False
