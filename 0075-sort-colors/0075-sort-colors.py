cd293f8a50e8cf02be7d6060f8358e0b562c5d03
ruth987 <99682266+ruth987@users.noreply.github.com>
2022-10-18 06:47:31 -0700

    Time: 65 ms (40.06%), Space: 14 MB (15.28%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,23 +1,23 @@
 class Solution:
     def sortColors(self, nums):
-        def swap(arr, index1, index2):
-            arr[index1], arr[index2] = arr[index2], arr[index1]
-            return arr
+        start, end = 0, len(nums)-1 #for zeros and twos.
+        i = 0 # for the ones.
         
-        left, right = 0, len(nums)-1
-        i = 0 # a pointers to go through the array
+        def swap(nums, index1, index2):
+            nums[index1], nums[index2] = nums[index2], nums[index1]
+            return nums
         
-        while i <= right:
-            if nums[i]==0:
-                swap(nums, i, left)
-                left+=1
+        while i<=end:
+            if nums[i] == 0:
+                swap(nums, i, start)
+                start += 1
             elif nums[i]==2:
-                swap(nums, i, right)
-                right-=1
+                swap(nums, i, end)
+                end-=1
                 i-=1
             i += 1
-            
-        return nums
+        print(nums)
+        
 
         
             
