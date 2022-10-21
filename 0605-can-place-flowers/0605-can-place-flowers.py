4e834aa8c7dd82cc6032841dbaa5dc01a057a849
ruth987 <99682266+ruth987@users.noreply.github.com>
2022-10-21 17:54:24 +0300

    Time: 370 ms (45.33%), Space: 14.5 MB (30.86%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,25 +1,18 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        possible = 0
-        stack = []
-        if len(flowerbed)==1 and flowerbed[0]==0:
-            return 1>=n
-        
-        for index in range(len(flowerbed)):
-            if flowerbed[index]==1:
-                stack.append(flowerbed[index])
-            else: # if flowerbed[index] == 0
-                if not stack and flowerbed[index+1]==0:
-                    possible += 1
-                    stack.append(1)
-                elif stack and stack[-1]==1:
-                    stack.append(flowerbed[index])
-                elif stack and stack[-1]==0 and index == len(flowerbed)-1:
-                    possible += 1
-                    stack.append(1)
-                elif stack and stack[-1]==0 and flowerbed[index+1]==0:
-                    possible += 1
-                    stack.append(1)
-                    
-        return possible >= n
+        count, prev = 0, 0
+
+        for cur in flowerbed:
+            if cur == 1:
+                if prev == 1: # violation!
+                    count -= 1
+                prev = 1
+            else:
+                if prev == 1: # can't plant
+                    prev = 0 
+                else:
+                    count += 1
+                    prev = 1 # the cur plot gets taken
+            
+        return count >= n
                 
