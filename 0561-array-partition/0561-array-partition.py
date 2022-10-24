6d6a1f01dd236271510dd1ae9d073d07d91862a8
ruth987 <99682266+ruth987@users.noreply.github.com>
2022-10-24 16:50:51 +0300

    Time: 653 ms (34.72%), Space: 16.8 MB (56.20%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -1,9 +1,9 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        nums.sort(reverse = True)
+        nums.sort()
         max_sum = 0
         for idx in range(len(nums)):
-            if idx % 2 != 0:
+            if idx % 2 == 0:
                 max_sum += nums[idx]
                 
         return max_sum
