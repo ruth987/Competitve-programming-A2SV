b0bce36cb34367a0d590069fd061e6a21e19d1ba
ruth987 <99682266+ruth987@users.noreply.github.com>
2022-10-30 16:51:02 +0300

    Time: 904 ms (9.56%), Space: 16.6 MB (74.64%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,25 +1,22 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        for idx in range(1, len(nums)):
-            nums[idx] += nums[idx-1]
-        # [1,0,0]   
-        count = 0   
+        #a dictionary to store what we had before and how many times
         mydict = {0:1}
-        
+        #how many subarrays have a sum that equals k
+        count = 0
+        #prefix sum
+        for idx in range(1,len(nums)):
+            nums[idx] += nums[idx-1]
+            
         for number in nums:
+            #if number-k in dictionary currentnumber -(number-k) == k 
             if number-k in mydict:
-                count+= mydict[number-k]
-            if number not in mydict:
-                mydict[number] = 1
-                
+                count += mydict[number-k]
+            if number in mydict:
+                mydict[number]+=1
             else:
-                mydict[number] += 1
+                mydict[number] = 1
         return count
-
-                
-                
-        
-        
         
         
         
