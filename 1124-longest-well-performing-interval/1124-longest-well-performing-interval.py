09fa2a54dace03d30f58392217393b75107297eb
ruth987 <99682266+ruth987@users.noreply.github.com>
2022-11-08 01:16:31 +0300

    Time: 538 ms (34.58%), Space: 14.5 MB (97.20%) - LeetHub

--- 1124-longest-well-performing-interval/1124-longest-well-performing-interval.py
+++ 1124-longest-well-performing-interval/1124-longest-well-performing-interval.py
@@ -1,22 +1,15 @@
 class Solution:
     def longestWPI(self, hours: List[int]) -> int: 
-        #changing tiring days to +1 and the others to -1
-        for idx in range(len(hours)):
-            hours[idx] = 1 if hours[idx]>8 else -1
+        prefixSum=0
+        hmap={}
+        ans=0
         
-        #prefix sum of the hours array
-        for idx in range(1, len(hours)):
-            hours[idx] += hours[idx-1]
-        
-        #finding the longest well-performing interval
-        max_len = 0
-        mydict = {}
-        for i,hr in enumerate(hours):
-            if hr > 0:
-                max_len = max(max_len, i+1)
-            if hr-1 in mydict:
-                max_len = max(max_len, i-mydict[hr-1])
-            if hr not in mydict:
-                mydict[hr] = i
-                
-        return max_len
+        for length,hour in enumerate(hours):
+            prefixSum+=1 if hour>8 else -1
+            if prefixSum>0:ans=max(ans,length+1)
+            if prefixSum not in hmap:
+                hmap[prefixSum]=length
+            if prefixSum-1 in hmap:
+                ans=max(ans,length-hmap[prefixSum-1])
+        return ans
+   
