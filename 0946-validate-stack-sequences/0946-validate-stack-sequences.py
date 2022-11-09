764a6bdd3b01c8d7d7e785099543704dbee452cb
ruth987 <99682266+ruth987@users.noreply.github.com>
2022-11-09 15:50:39 +0300

    Time: 182 ms (17.33%), Space: 14.1 MB (44.73%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.py
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.py
@@ -3,13 +3,11 @@ class Solution:
             stack, p_idx = [], 0
             
             for num in pushed:
+                stack.append(num)
                 while stack and p_idx<len(popped) and stack[-1] == popped[p_idx]:
                     stack.pop()
                     p_idx += 1
-                stack.append(num)
-            while stack and p_idx<len(popped) and stack[-1] == popped[p_idx]:
-                    stack.pop()
-                    p_idx += 1    
+
             if stack:
                 return False
             return True
