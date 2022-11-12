4aaaf881fda0d37835324efceb1ae822a9b27e7e
ruth987 <99682266+ruth987@users.noreply.github.com>
2022-11-12 17:15:17 +0300

    Time: 847 ms (93.50%), Space: 29.7 MB (80.28%) - LeetHub

---
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -0,0 +1,82 @@
+class Solution:
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        seen = set()
+        sm = 0 
+        ans = 0
+        right = 0 
+        left = 0 
+        while right < len(nums): 
+            if len(seen) != k:
+                if nums[right] not in seen: 
+                    seen.add(nums[right])
+                    sm += nums[right]
+                    right+=1 
+                else: 
+                    seen.remove(nums[left])
+                    sm -= nums[left]
+                    left += 1 
+            else: 
+                ans = ans if ans > sm else sm
+                seen.remove(nums[left])
+                sm -= nums[left]
+                left += 1
+
+
+        if len(seen) == k: 
+            ans = ans if ans > sm else sm
+        return ans 
+        """
+        left, right = 0, k-1
+        seen = set()
+        sub_sum = 0
+        
+        for idx in range(k-1):
+            seen.add(nums[idx])
+            sub_sum += nums[idx]
+        max_sum = 0
+        
+        while right < len(nums):
+            sub_sum += nums[right]
+            seen.add(nums[right])
+            
+            if len(seen)==k:
+                max_sum = max(max_sum, sub_sum)
+            
+            left+=1
+            right+=1
+            seen.remove(nums[left-1])
+            sub_sum -= nums[left-1]
+        return max_sum
+        
+        """
+        
+    """
+    class Solution:
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        seen = set()
+        sm = 0 
+        ans = 0
+        right = 0 
+        left = 0 
+        while right < len(nums): 
+            if len(s) != k:
+                if nums[right] not in seen: 
+                    seen.add(nums[right])
+                    sm += nums[right]
+                    right+=1 
+                else: 
+                    seen.remove(nums[left])
+                    sm -= nums[left]
+                    left += 1 
+            else: 
+                ans = ans if ans > sm else sm
+                s.remove(nums[left])
+                sm -= nums[left]
+                left += 1
+
+
+        if len(seen) == k: 
+            ans = ans if ans > sm else sm
+        return ans 
+    
+    """
