0026e70195116678a6cef939b11895961a3e786c
ruth987 <99682266+ruth987@users.noreply.github.com>
2022-11-15 20:46:52 +0300

    Time: 58 ms (38.85%), Space: 14 MB (11.66%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,18 +1,18 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        s = s.split()
-        s = sorted(s, key = lambda word:word[-1])
-        n = len(s)
-        answer = ""
-        x = 0
-        
-        for word in s:
-            x+=1
-            for letter in word: 
-                if not letter.isdigit():
-                    answer += letter
-            if x != n:
-                answer += " "
-        return answer
+        bucket = [""]*10
+
+        s = s.split(" ")
+
+        for wr in s:
+            bucket[int(wr[-1])] += wr
+
+        res = ""
+
+        for wr in bucket:
+            if wr != "":
+                res += wr[:-1] + " "
+
+        return res[:-1]
         
         
