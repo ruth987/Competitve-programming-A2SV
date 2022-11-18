cb1794bddaf161e9d28caaa404b0c37ec41953d0
ruth987 <99682266+ruth987@users.noreply.github.com>
2022-11-18 10:03:35 +0300

    Time: 2171 ms (17.29%), Space: 46.9 MB (22.12%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -6,10 +6,9 @@
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         arr = []
-        current = head
-        while current:
-            arr.append(current.val)
-            current = current.next
+        while head:
+            arr.append(head.val)
+            head = head.next
         
         left, right = 0, len(arr)-1
         while left <= right:
