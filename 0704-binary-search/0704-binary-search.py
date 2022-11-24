784e328bfabe0ef21bd4b05a77754e92aab1cee0
ruth987 <99682266+ruth987@users.noreply.github.com>
2022-11-24 21:58:24 +0300

    Time: 462 ms (66.76%), Space: 15.5 MB (73.91%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,19 +1,17 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        """
-        nums = is a sorted array.
-        target= is the number we are searching for.
+        left=0
+        right=len(nums)-1
         
-        if we find the target then return the index else return -1
-        
-        so the binary search algorithm time comp: o(log n)
-        
-        the brute force approach is going through all the elements of the array.
-        the time comp: o(n)
-        space comp: o(1)
-        """
-        for idx in range(len(nums)):
-            if nums[idx] == target:
-                return idx
+        while(left<= right):
+            mid=(left+right)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid] < target:
+                 left=mid+1
+            else:
+                right=mid-1
+                
         return -1
-        
+
+            
