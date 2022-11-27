32b9e4c1e89b70203454680a3665d29460ba6cb3
ruth987 <99682266+ruth987@users.noreply.github.com>
2022-11-27 15:06:20 +0300

    Time: 118 ms (70.33%), Space: 26 MB (85.82%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -1,21 +1,8 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        """
-        letters in the dna sequence are : A C G T
-        
-        - the substrings in the output list has to be unique.
-        - the substrings has to occur more than once.
-        
-        Input: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
-        
-        we can use a set then if the substring is already in the set then append the
-        substring into the output array if not then add it to the set.
-        
-        two pointers: left and right ...we store the substring in between in a hashmap
-        """
         left, right = 0, 10
-        aset = set()
-        output = []
+        aset, output = set(), []
+        
         
         while right < len(s):
             string = s[left:right]
@@ -25,11 +12,10 @@ class Solution:
                 aset.add(string)
             left+=1
             right+=1
-        string = s[left:right]
-        if string in aset:
-            output.append(string)
-        else:
-            aset.add(string)
+            
+        if s[left:right] in aset:
+            output.append(s[left:right])
+
         return list(set(output))
                 
                 
