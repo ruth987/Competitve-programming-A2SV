aa27614223aa21dffb4ecc061b2a6ff4e3d8bcbb
ruth987 <99682266+ruth987@users.noreply.github.com>
2022-11-28 20:25:50 +0300

    Time: 4196 ms (60.60%), Space: 68.8 MB (58.91%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -1,5 +1,6 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        """i got into a2sv yeeeee"""
         winners = [match[0] for match in matches]
         losers = [match[1] for match in matches]
         perfect_winners = list(set(winners) - set(losers))
