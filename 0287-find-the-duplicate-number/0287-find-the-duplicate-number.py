bb0052c131772328a04c1c8f1819fdd20ef01de7
ruth987 <99682266+ruth987@users.noreply.github.com>
2022-12-03 20:16:31 +0300

    Time: 1739 ms (23.86%), Space: 27.8 MB (98.11%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,18 +1,23 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         """
-        we can simply do this problem using o(n) space complexity but the triccky part 
-        is how can we do it using o(1) space complexity??
-        
-        so how can we do it without using extra space?
-        this approach makes the time complexity a little slower because we are sorting
-        the array but the space complexity becomes constant times.
+        to make space comp o(1):
+        - use linked list concept
+        - use floyd's algorithm
         
         """
-        nums.sort()
+        slow, fast = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
         
-        for idx in range(1, len(nums)):
-            if nums[idx]==nums[idx-1]:
-                return nums[idx]
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
             
         
