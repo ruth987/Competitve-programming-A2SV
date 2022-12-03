58e9f96311516d74a7ebe83281413d634e2e1312
ruth987 <99682266+ruth987@users.noreply.github.com>
2022-12-03 13:27:48 +0300

    Time: 265 ms (82.62%), Space: 16.4 MB (17.63%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,16 +1,37 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        """
-        what is the most obvious approach??
-        1, squaring all the elements
-        2, returning the sorted array of the squares 
-        we can do this in different ways?
-        - sort built in function 
-        - using some other sorting algorithms
-        
-        """
+        left, right = len(nums)-1, len(nums)
         for idx in range(len(nums)):
-            nums[idx] = nums[idx]**2
-        #print(nums)
-        return sorted(nums)
+            if nums[idx] == 0 or nums[idx]>0:
+                left, right = idx-1, idx
+                break
+
+        answer = []              
+        while left > -1 and right < len(nums):
+            if abs(nums[left]) > abs(nums[right]):
+                answer.append(nums[right]**2)
+                right+=1
+            elif abs(nums[left]) < abs(nums[right]):
+                answer.append(nums[left]**2)
+                left-=1
+            else: #if they are equal.
+                answer.append(nums[left]**2)
+                answer.append(nums[right]**2)
+                left-=1
+                right+=1
+        
+        while left > -1:
+            answer.append(nums[left]**2)
+            left-=1
+            
+        while right < len(nums):
+            answer.append(nums[right]**2)
+            right+=1
+            
+        return answer
+        
+        
+        
+        
+        
         
