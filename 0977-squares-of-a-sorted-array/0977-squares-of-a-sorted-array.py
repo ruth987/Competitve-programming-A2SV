300ba35a9b11a29eb8819837279b08e369e135ed
ruth987 <99682266+ruth987@users.noreply.github.com>
2022-12-03 13:35:00 +0300

    Time: 220 ms (95.68%), Space: 16.3 MB (17.63%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,37 +1,15 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        left, right = len(nums)-1, len(nums)
-        for idx in range(len(nums)):
-            if nums[idx] == 0 or nums[idx]>0:
-                left, right = idx-1, idx
-                break
-
-        answer = []              
-        while left > -1 and right < len(nums):
-            if abs(nums[left]) > abs(nums[right]):
-                answer.append(nums[right]**2)
-                right+=1
-            elif abs(nums[left]) < abs(nums[right]):
-                answer.append(nums[left]**2)
-                left-=1
-            else: #if they are equal.
-                answer.append(nums[left]**2)
-                answer.append(nums[right]**2)
-                left-=1
-                right+=1
+        left, right = 0, len(nums)-1
+        answer = []
         
-        while left > -1:
-            answer.append(nums[left]**2)
-            left-=1
-            
-        while right < len(nums):
-            answer.append(nums[right]**2)
-            right+=1
-            
-        return answer
-        
-        
-        
-        
-        
+        while left <= right:
+            if nums[left]*nums[left] > nums[right]*nums[right]:
+                answer.append(nums[left]*nums[left])
+                left+=1
+            else:
+                answer.append(nums[right]*nums[right])
+                right-=1
+                
+        return answer[::-1]
         
