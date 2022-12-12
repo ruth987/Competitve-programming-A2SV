44b6b1a7f6d33164760fdde24e9aef8d2b434bca
ruth987 <99682266+ruth987@users.noreply.github.com>
2022-12-12 21:41:44 +0300

    Time: 59 ms (48.65%), Space: 13.8 MB (64.12%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -5,15 +5,15 @@ class Solution:
         we can use minHeap then take the last two elements and collide them 
         then heappush the new element then heapify the minHeap again.
         """
-        stones = [-weight for weight in stones]
-        heapq.heapify(stones)
+        myStones = [-weight for weight in stones]
+        heapq.heapify(myStones)
         
-        while len(stones) > 1:
-            s1 = heapq.heappop(stones)
-            s2 = heapq.heappop(stones)
+        while len(myStones) >= 2:
+            s1 = heapq.heappop(myStones)
+            s2 = heapq.heappop(myStones)
             if s2>s1:
-                heapq.heappush(stones, s1-s2)
-        if not stones:
+                heapq.heappush(myStones, s1-s2)
+        if not myStones:
             return 0
-        return abs(stones[0])
+        return abs(myStones[0])
             
