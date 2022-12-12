6445f04170afd41de50032ada63891bba45dbad7
ruth987 <99682266+ruth987@users.noreply.github.com>
2022-12-12 21:40:14 +0300

    Time: 63 ms (35.02%), Space: 13.9 MB (15.71%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,27 +1,19 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         """
-        the game is returning the last stone weight left.
-        
-        [2,7,4,1,8,1]
-        what if we sort it so that we can know who to collide with who ?
-        
-        ohh we choose the heaviest two stones to smash them together.
-        so lets sort the array and pick the last two elements then smash it.
-        
-        [1,1,2,4,7,8]
-        i am thinking about using stack. after sorting the arrau in ascending order.
-        the brute force approach is as followed: 
+        the optimized using heap: should we use min heap or max heap?
+        we can use minHeap then take the last two elements and collide them 
+        then heappush the new element then heapify the minHeap again.
         """
-        stack = sorted(stones)
+        stones = [-weight for weight in stones]
+        heapq.heapify(stones)
         
-        while len(stack)>=2:
-            s1 = stack.pop()
-            s2 = stack.pop()
-            if s1!=s2:
-                stack.append(abs(s1-s2))
-            stack.sort()
-        if not stack:
+        while len(stones) > 1:
+            s1 = heapq.heappop(stones)
+            s2 = heapq.heappop(stones)
+            if s2>s1:
+                heapq.heappush(stones, s1-s2)
+        if not stones:
             return 0
-        return stack.pop()
-        
+        return abs(stones[0])
+            
