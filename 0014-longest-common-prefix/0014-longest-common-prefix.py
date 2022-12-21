bbbc63749be37e3d08f8705daff9f9ac34be7f91
ruth987 <99682266+ruth987@users.noreply.github.com>
2022-12-21 08:36:36 -0800

    Time: 85 ms (11.91%), Space: 14 MB (12.02%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,20 +1,25 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        """
-        strs = ["flow", "flower","flight"]
+        first = strs[0]
         
-        i refers the internal word index
-        idx refers the list index
-        """
-        answer = ""
-        strs = sorted(strs, key = lambda word: len(word))
-        print(strs)
-        for i in range(len(strs[0])):
-            for word in strs:
-                if word[i] != strs[0][i]:
-                    return answer
-            answer += strs[0][i]
-        return answer
-                    
+        for word in strs:
+            if first > word:
+                first = word
+                
+        last = strs[0]
         
-            
+        for word in strs:
+            if last < word:
+                last = word
+        print(first, last)
+        ans = ""
+        i, j = 0, 0
+        if len(strs) == 1:
+            return strs[0]
+        while i<len(first) and j<len(last) and first[i] == last[j]:
+            ans += first[i]
+            i+=1
+            j+=1
+        return ans
+        
+        
