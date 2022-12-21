9180077678e8580a533f47e1fd12a7122662b9a6
ruth987 <99682266+ruth987@users.noreply.github.com>
2022-12-21 03:43:09 -0800

    Time: 189 ms (6.97%), Space: 13.9 MB (59.90%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,3 +1,22 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        return str(x) == str(x)[::-1]
+        # solve it without converting into a string
+        # we can use two pointers to check that.
+        if x<0:
+            return False
+        
+        div = 1
+        while x >= 10*div:
+            div *= 10
+        
+        while x:
+            right = x%10
+            left = x//div
+            
+            if left!=right:
+                return False
+            
+            x = (x%div)//10
+            div = div/100
+        return True
+    
