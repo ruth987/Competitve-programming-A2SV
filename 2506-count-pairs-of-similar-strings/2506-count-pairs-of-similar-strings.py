551361b6fce71aec708bdd98399dacf7cb9a7d1a
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-01-03 04:20:58 -0800

    Time: 947 ms (13.61%), Space: 13.9 MB (97.14%) - LeetHub

--- 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.py
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.py
@@ -1,20 +1,7 @@
 class Solution:
     def similarPairs(self, words: List[str]) -> int:
-        """
-        words = ["aba","aabb","abcd","bac","aabc"]
-        
-        similar words: words which have same characters
-        we have to use hashtable to count all the possible pairs.
-        
-        1, changing each into a set then storing it in a dictionary if it doesnt already
-        exist.
-        2, if it is already there just increament the count in the dictionary
-        
-        3, when ever it is already there before increamenting increase the answer by the
-        number of count of that word in the dictionary
-        
-        """
         count = 0
+        
         for idx in range(len(words)):
             for i in range(idx+1, len(words)):
                 if set(words[idx]) == set(words[i]):
