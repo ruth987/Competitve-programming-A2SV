843f68a272bd0782281140c6b2d5a9a0fe97b2c9
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-01-12 07:27:27 -0800

    Time: 159 ms (84.67%), Space: 14.7 MB (23.40%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -1,24 +1,15 @@
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
-        """
-        n strings --> have same length
-        
-        
-        """
         count = 0
         n = len(strs[0])
         the_strings = []
         
         for idx in range(n):
-            poss = ""
-            for word in strs:
-                poss+= word[idx]
-            the_strings.append(poss)
-        
-        for word in the_strings:
-            if word!= "".join(sorted(word)):
-                count += 1
-        
+            for index in range(1,len(strs)):
+                if strs[index][idx]<strs[index-1][idx]:
+                    count += 1
+                    break
+                
         return count 
             
             
