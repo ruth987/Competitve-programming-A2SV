4a3c22a1cc0caac491b753e9393d83f1fb7300ba
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-01-13 06:46:37 -0800

    Time: 89 ms (82.77%), Space: 13.7 MB (98.42%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,7 +1,24 @@
 class Solution(object):
     def isToeplitzMatrix(self, matrix):
-        return all(r == 0 or c == 0 or matrix[r-1][c-1] == val
-                   for r, row in enumerate(matrix)
-                   for c, val in enumerate(row))
-            
-            
+        #from row highest to row lowest
+        for r in range(len(matrix)-1,-1,-1):
+            c = 0
+            val = matrix[r][c]
+            for row in range(r, len(matrix)):
+                if matrix[row][c] != val:
+                    return False
+                c+=1
+                if c >= len(matrix[0]):
+                    break
+
+        #from col lowest to col highest
+        for c in range(1, len(matrix[0])):
+            r = 0
+            val = matrix[r][c]
+            for col in range(c, len(matrix[0])):
+                if matrix[r][col] != val:
+                     return False
+                r+=1
+                if r >= len(matrix):
+                    break
+        return True
