034babe4fcf9d268ca938445837e5fb094c0b94e
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-01-14 10:43:03 -0800

    Time: 60 ms (71.05%), Space: 14.2 MB (59.51%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
@@ -1,22 +1,20 @@
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
-        #apply the first operations
         stack = [nums[0]]
-        here = 0
-        for idx in range(1,len(nums)):
-            if nums[idx]==nums[idx-1] and here == 0:
-                stack.pop()
-                stack.append(nums[idx]*2)
-                stack.append(0)
-                here = 1
-            else:
-                here = 0
-                stack.append(nums[idx])
-        #put zeros at the end
-        x = 0
-        for idx in range(len(stack)):
-            if stack[idx]!=0:
-                stack[idx], stack[x] = stack[x], stack[idx]
-                x += 1
-        return stack
         
+        for idx in range(1,len(nums)):
+            if nums[idx] != stack[-1]:
+                stack.append(nums[idx])
+            else: # if it is the same
+                element = stack.pop()
+                stack.append(2*element)
+                stack.append(0)
+        
+        #move zeros
+        i = 0
+        for index in range(len(stack)):
+            if stack[index]!=0:
+                stack[i], stack[index] = stack[index], stack[i]
+                i += 1
+        return stack
+     
