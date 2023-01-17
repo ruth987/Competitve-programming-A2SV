e07152369c8727e9b841891b164d5592a36df0f9
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-01-17 07:43:12 -0800

    Time: 622 ms (10.21%), Space: 14.4 MB (84.60%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,12 +1,23 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        """
-        from big height to small height
-        """
-        mydict = list(zip(names, heights))
-        mydict = sorted(mydict, key = lambda item:-item[1])
-        ans = []
-        for item in mydict:
-            ans.append(item[0])
         
-        return ans
+#         for idx in range(len(names)-1, -1, -1):
+#             min_val = 10**6
+#             index = 0
+#             for i in range(len(heights)):
+#                 if heights[i]<min_val:
+#                     index = i
+#                     min_val = heights[i]
+#             heights[idx], heights[index] = heights[index] ,heights[idx]
+#             names[idx], names[index] = names[index] ,names[idx]
+        
+#         return names
+
+        for idx in range(len(names)):
+            index =idx
+            for i in range(idx,len(names)):
+                if heights[i] > heights[index]:
+                    index = i
+            heights[index], heights[idx] = heights[idx], heights[index]
+            names[index], names[idx] = names[idx], names[index]
+        return names
