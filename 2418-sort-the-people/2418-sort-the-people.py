43a69144a1ccad935683222a7425e0eebb7b6684
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-01-17 08:51:19 -0800

    Time: 183 ms (53.22%), Space: 14.4 MB (84.60%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,15 +1,19 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        # sorted and unsorted part
-        for idx in range(1, len(heights)):
-            if heights[idx] > heights[idx-1]:
-                val = heights[idx]
-                index = idx
-                for i in range(idx-1,-1,-1):
-                    if val>heights[i]:
-                        heights[i],heights[index] = heights[index],heights[i]
-                        names[i],names[index] = names[index],names[i]
-                        index -= 1
+        gap = len(names) // 2
         
-        return names
-                        
+        while gap > 0:
+            j = gap
+            while j < len(names):
+                i = j - gap
+                while i >= 0:
+                    if heights[i+gap] < heights[i]:
+                        break
+                    else:
+                        heights[i+gap], heights[i] = heights[i],heights[i+gap]
+                        names[i+gap], names[i] = names[i], names[i+gap]
+                    i-=gap
+                j+=1
+            
+            gap = gap//2
+        return names 
