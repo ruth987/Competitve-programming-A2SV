f79fef01c92b25559eb3be43ac01c71c85cbee02
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-01-17 08:13:47 -0800

    Time: 1221 ms (7.16%), Space: 14.4 MB (42.89%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,23 +1,15 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        # sorted and unsorted part
+        for idx in range(1, len(heights)):
+            if heights[idx] > heights[idx-1]:
+                val = heights[idx]
+                index = idx
+                for i in range(idx-1,-1,-1):
+                    if val>heights[i]:
+                        heights[i],heights[index] = heights[index],heights[i]
+                        names[i],names[index] = names[index],names[i]
+                        index -= 1
         
-#         for idx in range(len(names)-1, -1, -1):
-#             min_val = 10**6
-#             index = 0
-#             for i in range(len(heights)):
-#                 if heights[i]<min_val:
-#                     index = i
-#                     min_val = heights[i]
-#             heights[idx], heights[index] = heights[index] ,heights[idx]
-#             names[idx], names[index] = names[index] ,names[idx]
-        
-#         return names
-
-        for idx in range(len(names)):
-            index =idx
-            for i in range(idx,len(names)):
-                if heights[i] > heights[index]:
-                    index = i
-            heights[index], heights[idx] = heights[idx], heights[index]
-            names[index], names[idx] = names[idx], names[index]
         return names
+                        
