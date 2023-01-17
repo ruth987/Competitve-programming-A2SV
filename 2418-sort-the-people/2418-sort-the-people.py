b96b6ef44470442148f92b94243d3e36daebbbbc
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-01-17 07:14:55 -0800

    Time: 110 ms (97.07%), Space: 14.5 MB (42.89%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -3,18 +3,10 @@ class Solution:
         """
         from big height to small height
         """
-        # mydict = dict(zip(names, heights))
-        # print(mydict)
-        i = 0
-        while i < len(heights)-1:
-            flag = False
-            for idx in range(len(heights)-1):
-                if heights[idx] < heights[idx+1]:
-                    heights[idx], heights[idx+1] = heights[idx+1], heights[idx]
-                    names[idx], names[idx+1] = names[idx+1], names[idx]
-                    flag = True
-            if flag == False:
-                break
-            i += 1
-            
-        return names  
+        mydict = list(zip(names, heights))
+        mydict = sorted(mydict, key = lambda item:-item[1])
+        ans = []
+        for item in mydict:
+            ans.append(item[0])
+        
+        return ans
