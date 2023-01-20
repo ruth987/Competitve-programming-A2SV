ec1438bae2f37b6d87f68bf32e14e816074bc6f6
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-01-19 23:42:20 -0800

    Time: 36 ms (75.07%), Space: 13.8 MB (96.81%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,19 +1,16 @@
 class Solution:
     def sortColors(self, nums):
+        i = 0
         start, end = 0, len(nums)-1
-        i = 0 # the index that goes through the array
         
         while i <= end:
             if nums[i] == 0:
-                nums[i], nums[start] = nums[start], nums[i]
-                start+=1
+                nums[start], nums[i] = nums[i], nums[start]
+                start += 1
             elif nums[i] == 2:
-                nums[i], nums[end] = nums[end], nums[i]
-                end-=1
-                i-=1
-            i+=1                            
-        print(nums)
+                nums[end], nums[i] = nums[i], nums[end]
+                end -= 1
+                i -= 1
+            i += 1
 
-            
-        
-        
+        print(nums)
