1c74468e7bc9ceebd7e80f3b197ac47a9f5be265
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-01-20 00:32:08 -0800

    Time: 46 ms (71.20%), Space: 14 MB (27.72%) - LeetHub

--- 0969-pancake-sorting/0969-pancake-sorting.py
+++ 0969-pancake-sorting/0969-pancake-sorting.py
@@ -1,12 +1,23 @@
 class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
-        n = len(arr)
-        k = []
-        for idx in range(len(arr)):
-            max1 = max(arr[:n-idx])
-            max_idx = arr.index(max1) + 1
-            arr[:max_idx] = reversed(arr[:max_idx])
-            k.append(max_idx)
-            arr[:n-idx] = reversed(arr[:n-idx])
-            k.append(n-idx)
-        return k
+        ans = []
+        i = len(arr) - 1
+        
+        while i > 0:
+            while i > 0 and arr[i] - 1 == i:
+                i -= 1
+            
+            if i == 0:
+                break
+            
+            index = arr.index(i + 1)
+            
+            if index > 0: # if the max val is already at index 0, we dont need to swap
+                arr[:index + 1] = reversed(arr[:index + 1])
+                ans.append(index + 1)
+            
+            arr[:i + 1] = reversed(arr[:i + 1])
+            ans.append(i + 1)
+            i -= 1
+        
+        return ans
