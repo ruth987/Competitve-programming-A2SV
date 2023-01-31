d4f40cc3ea685b515a82f7390288597db5a65850
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-01-31 07:14:50 -0800

    Time: 33 ms (90.67%), Space: 15.4 MB (50.95%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,16 +5,13 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        """
-        emp-->1-->2-->3-->4-->5-->emp
-             prev cur  curr.next
-
-        """
-        previous, current = None, head
-        while current: #i want the while loop to keep going as long as the curr isnot null
-            nxt = current.next
-            current.next = previous
-            previous = current
-            current = nxt
+        curr = head
+        prev = None
         
-        return previous
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+
+        return prev
