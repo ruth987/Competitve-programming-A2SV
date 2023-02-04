407042cd064814531f96b0416d06f79ca36e2ac4
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-02-04 11:27:40 -0800

    Time: 1399 ms (24.07%), Space: 14 MB (28.54%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,10 +1,13 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        mydict = collections.Counter(s1)
+                
         left, right = 0, len(s1)
-        while right <= len(s2):
-            if sorted(s2[left:right]) == sorted(s1):
+        while right < len(s2)+1:
+            yourdict = collections.Counter(s2[left:right])
+            if mydict == yourdict:
                 return True
-            left += 1
-            right += 1
+            else:
+                left+=1
+                right+=1
         return False
-    
