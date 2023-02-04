c10dc7476da1328c8ab90f8a712dbb96d39fd63b
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-02-04 11:29:19 -0800

    Time: 222 ms (37.72%), Space: 14 MB (64.59%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,13 +1,13 @@
+from collections import Counter
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        mydict = collections.Counter(s1)
-                
-        left, right = 0, len(s1)
-        while right < len(s2)+1:
-            yourdict = collections.Counter(s2[left:right])
-            if mydict == yourdict:
+        s1_counter = Counter(s1)
+        length = len(s1)
+        for i in range(len(s2)):
+            if s2[i] in s1_counter:
+                s1_counter[s2[i]] -= 1
+            if i >= length and s2[i - length] in s1_counter:
+                s1_counter[s2[i - length]] += 1
+            if all([s1_counter[i] == 0 for i in s1_counter]): 
                 return True
-            else:
-                left+=1
-                right+=1
         return False
