73a09871f543dfc522d093c1114613b25f4e578c
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-02-19 19:29:50 +0300

    Time: 53 ms (19.33%), Space: 14.3 MB (48.53%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,41 +1,18 @@
 class Solution:
-    def findPivot(self,nums):
-            start=0
-            end = len(nums)-1
-
-            while start< end:
-                mid = start+(end-start)//2
-                if nums[mid]>nums[mid+1] and mid<=end:
-                    return mid  
-                if nums[mid]<nums[mid-1]:
-                    return mid-1
-                if nums[start]>=nums[mid]:
-                    end = mid-1
-                else:
-                    start=mid+1
-
-            return -1
-
-
-    def binarySearch(self,arr,target,start,end):
-        while (start<=end):   
-            mid= start+(end-start)//2
-            if target==arr[mid] :
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
                 return mid
-            elif target < arr[mid]:
-                end=mid-1
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
             else:
-                start= mid+1
-        return -1  
-            
-
-    def search(self, nums, target):
-        pivot=self.findPivot(nums)                  
-        if pivot==-1:                             
-            return self.binarySearch(nums,target,0,len(nums)-1)
-        if nums[pivot]==target:
-            return pivot
-        elif target>=nums[0]:                 
-            return self.binarySearch(nums,target,0,pivot-1)
-        else:                                     
-            return self.binarySearch(nums,target,pivot+1,len(nums)-1) 
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return -1
