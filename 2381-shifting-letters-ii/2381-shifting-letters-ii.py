554b89491cf9b50634e78e18fafb91cd2f166560
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-02-26 18:10:52 +0300

    Time: 1267 ms (100.00%), Space: 39.1 MB (77.62%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.py
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -2,12 +2,9 @@ class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
         # save the ascii values of each letters
         
-        arr1 = []
-        arr2 = []
-        for letter in s:
-            arr1.append(ord(letter))
-            arr2.append(0)
-        arr2 = [0]+arr2
+        arr1 = list(s)
+        arr2 = [0]*(len(arr1)+1)
+
         # create an array with zero eleemnts to see the effect of shifting
         for start,end,direction in shifts:
             if direction == 0:
@@ -19,24 +16,11 @@ class Solution:
            
         
         # find prefix sum of the shifting array
-        for idx in range(1, len(arr2)):
-            arr2[idx] += arr2[idx-1]
+        arr2 = list(accumulate(arr2))
             
-        # add the ascii array and the second array together
-        for idx in range(len(arr1)):
-            arr1[idx] += arr2[idx]
-            
-        # find the element after checking if the ascii value is between 97 and 122
         ans = []
-        for val in arr1:
-            if val >= 97 and val <= 122:
-                ans.append(chr(val))
-        # if it is not then substract 26 till you get that
-            else:
-                while val < 97:
-                    val += 26
-                while val > 122:
-                    val -= 26
-                ans.append(chr(val))
-                
+        for idx in range(len(arr2)-1):
+            result = (((ord(s[idx])-97)+arr2[idx])%26)+97
+            ans.append(chr(result))
+            
         return "".join(ans)
