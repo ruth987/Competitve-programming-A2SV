71d9d604a25e7f2766cb7eddb6b1aaf86c9c5d56
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-02-27 16:45:08 +0300

    Time: 500 ms (60.69%), Space: 18.8 MB (85.01%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -2,6 +2,7 @@ class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
         stack = [-1, 0]
         sum_ = 0
+        
         for idx in range(1, len(arr)):
             while len(stack) >= 2 and arr[idx] < arr[stack[-1]]:
                 sum_ += abs((idx-stack[-1])*(stack[-1]-stack[-2]))* arr[stack[-1]]
@@ -9,12 +10,9 @@ class Solution:
                 
             stack.append(idx)
             
-        ln = len(arr)
-        stack.append(ln)
-        # print(stack)
-        # print(sum_)
+        stack.append(len(arr))
+
         for i in range(1, len(stack)-1):
-            sum_ += ((stack[i]-stack[i-1])*(ln-stack[i]))*arr[stack[i]]
-            # print(sum_)
+            sum_ += ((stack[i]-stack[i-1])*(len(arr) -stack[i]))*arr[stack[i]]
         
         return sum_%(10**9 + 7)
