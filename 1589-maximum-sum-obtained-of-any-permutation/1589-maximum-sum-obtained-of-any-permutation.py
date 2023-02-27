834c0edb55261e4641c898a6970fa3f4cc61ab81
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-02-27 06:46:13 +0300

    Time: 1619 ms (93.24%), Space: 48 MB (72.97%) - LeetHub

--- 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.py
+++ 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.py
@@ -6,22 +6,18 @@ class Solution:
             prefix[start] += 1
             prefix[end+1] -= 1
 
-        mydict = {0: prefix[0]}
-        
         for idx in range(1, len(prefix)):
             prefix[idx] += prefix[idx-1]
-            mydict[idx] = prefix[idx]
 
-        mydict = sorted(mydict.items(), key = lambda item: -item[1])
+        prefix.sort(reverse = True)
         nums.sort(reverse = True)
         ans = 0
         i = 0
 
-        for key,val in mydict:
-            if val == 0:
+        for i in range(len(nums)):
+            if prefix[i]==0:
                 break
-            ans += (val*nums[i])
-            i += 1
+            ans += (prefix[i]*nums[i])
         
         return ans% (10**9 + 7)
         
