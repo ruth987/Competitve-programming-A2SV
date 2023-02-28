a802d65ce0f8ac6852b18b663921bfdee38ca92e
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-02-28 15:16:38 +0300

    Time: 966 ms (32.50%), Space: 27 MB (14.58%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -3,6 +3,16 @@ class Solution:
         def findS(n):
             if n==1:
                 return "0"
-            return findS(n-1) +"1"+ "".join(["0" if i == "1" else "1" for i in (list(findS(n-1))[::-1])])
+            
+            var = list(findS(n-1))
+            new_var = var[:]
+            
+            for i in range(len(var)):
+                if new_var[i]=="1":
+                    new_var[i] = "0"
+                else:
+                    new_var[i] = "1"
+            new_var = new_var[::-1] 
+            return "".join(var) + "1"+ "".join(new_var)
         
         return (findS(n))[k-1]
