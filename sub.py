b4c9f54b70195ca0e686d40f140486d10a1d8574
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-03-02 20:24:09 +0300

    added sub.py

---
+++ sub.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        
+        mydict = {}
+        presum = 0
+        ans = 0
+        
+        for idx in range(len(nums)):
+            if presum not in mydict:
+                mydict[presum] = 1
+            else:
+                mydict[presum] += 1
+            
+            presum += nums[idx]
+            if presum - goal in mydict:
+                ans += mydict[presum-goal]
+                
+        return ans
