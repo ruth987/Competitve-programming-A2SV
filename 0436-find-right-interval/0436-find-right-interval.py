f29476e630ee338dda8ce5e55948ddd4f408ae7f
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-03-08 06:31:59 +0300

    Time: 270 ms (97.85%), Space: 19.8 MB (95.69%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.py
+++ 0436-find-right-interval/0436-find-right-interval.py
@@ -5,12 +5,9 @@ class Solution:
         new_i.sort()
         for idx in range(len(intervals)):
             intervals[idx].append(idx)
-            
-        intervals.sort(key= lambda item: item[0])
-        # print(intervals)
-        ln = len(intervals)
-        answer = [-1]*ln
-        
+
+        answer = [-1]*len(intervals)
+        intervals.sort(key=lambda item:item[0])
         for interval in intervals:
             val = bisect_left(new_i,interval[1])
             if val < len(intervals):
