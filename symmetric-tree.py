be925d92bbc970baf3692c68a87711b25562b1b5
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-03-08 07:52:19 +0300

    added symmetric-tree.python

---
+++ symmetric-tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        """ 
+        we have two sides: right subtree and left subtree
+        we compair the right side of the one with the left side of the other and vice versal
+        """
+        def isSym(root1, root2):
+            if not root1 and not root2:
+                return True
+            elif (not root1 and root2) or (not root2 and root1):
+                return False
+
+            if root1.val != root2.val:
+                return False
+
+            return (isSym(root1.left, root2.right) and isSym(root1.right, root2.left))
+
+        return isSym(root.left, root.right)
