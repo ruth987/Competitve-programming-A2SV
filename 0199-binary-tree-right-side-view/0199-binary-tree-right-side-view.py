3d4efc919e27c82c7f1517d1524d309713b75e27
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-03-12 20:10:17 +0300

    Time: 29 ms (89.64%), Space: 13.8 MB (97.12%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -6,25 +6,25 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
-            return []
-
-        mydict = {}
-        def calc_depth(root, dep):
-            if not root:
-                return 
-            
-            if dep in mydict:
-                mydict[dep].append(root.val)
-            else:
-                mydict[dep] = [root.val]
-
-            left = calc_depth(root.left, dep+1)
-            right = calc_depth(root.right, dep+1)
-            
-        calc_depth(root, 0)
         ans = []
-        for key,val in mydict.items():
-            ans.append(val[-1])
+        deq = deque([root])
+        
+        while deq:
+            rightside = None
+            deq_len = len(deq)
             
+            for i in range(deq_len):
+                node = deq.popleft()
+                
+                if node:
+                    rightside = node
+                    deq.append(node.left)
+                    deq.append(node.right)
+                    
+            if rightside:
+                ans.append(rightside.val)
+                    
         return ans
+                
+                
+            
