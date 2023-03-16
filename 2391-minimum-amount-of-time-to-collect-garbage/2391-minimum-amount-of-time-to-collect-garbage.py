a5dc8370be267f748b059fc3af724464e1e78001
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-03-16 15:33:04 +0300

    Time: 879 ms (88.95%), Space: 36.1 MB (80.83%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
@@ -1,35 +1,20 @@
 class Solution:
     def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
-        def pop_last_zeros(alist):
-            while alist and alist[-1] == 0:
-                alist.pop()
-
-        travel = [0]+list(accumulate(travel))
-
-        mydict = {"M":0, "G":0, "P":0}
-        m_list = []
-        p_list = []
-        g_list = []
-        for gar in garbage:
-            for letter in gar:
-                mydict[letter] += 1
+        l,m,p,g,res=0,0,0,0,0
+        for i in range(len(garbage)):
+            if 'M' in garbage[i] and m<i:
+                m=i
+            if 'P' in garbage[i] and p<i:
+                p=i
+            if 'G' in garbage[i] and g<i:
+                g=i
+            l+=len(garbage[i])
             
-            m_list.append(mydict["M"])
-            p_list.append(mydict["P"])
-            g_list.append(mydict["G"])
-            mydict = {"M":0, "G":0, "P":0}
+        if m!=0:
+            res=sum(travel[:m])
+        if p!=0:
+            res+=sum(travel[:p])
+        if g!=0:
+            res+=sum(travel[:g])
             
-        answer = 0
-        pop_last_zeros(m_list)
-        pop_last_zeros(p_list)
-        pop_last_zeros(g_list)
-        
-        if m_list:
-            answer += (travel[len(m_list)-1] + sum(m_list))
-        if p_list:
-            answer += (travel[len(p_list)-1] + sum(p_list))
-        if g_list:
-            answer += (travel[len(g_list)-1] + sum(g_list))
-        
-        return answer
-        
+        return res+l
