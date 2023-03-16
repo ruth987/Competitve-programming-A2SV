90db4073ee392ee73b274c20e14f7f704c69f5fa
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-03-16 15:23:36 +0300

    Time: 1010 ms (64.77%), Space: 37.4 MB (18.31%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
@@ -3,14 +3,9 @@ class Solution:
         def pop_last_zeros(alist):
             while alist and alist[-1] == 0:
                 alist.pop()
-        
-        # prefix sum for the travel array
-        travel = [0]+travel
-        for idx in range(1, len(travel)):
-            travel[idx] += travel[idx-1]
-        
-        
-        # print(travel)
+
+        travel = [0]+list(accumulate(travel))
+
         mydict = {"M":0, "G":0, "P":0}
         m_list = []
         p_list = []
@@ -23,11 +18,12 @@ class Solution:
             p_list.append(mydict["P"])
             g_list.append(mydict["G"])
             mydict = {"M":0, "G":0, "P":0}
+            
         answer = 0
-        # print(m_list, p_list, g_list)
         pop_last_zeros(m_list)
         pop_last_zeros(p_list)
         pop_last_zeros(g_list)
+        
         if m_list:
             answer += (travel[len(m_list)-1] + sum(m_list))
         if p_list:
