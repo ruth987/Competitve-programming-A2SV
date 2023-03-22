9a7007804cd0d6e4605d4ea5aac13d02d0878937
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-03-22 16:03:46 +0300

    added 132-pattern.python

---
+++ 132-pattern.py
@@ -0,0 +1,18 @@
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        if len(nums)<3:
+            return False
+      
+        min_val = nums[0]
+        stack = []
+
+        for idx in range(1,len(nums)):
+            while stack and nums[idx] >= stack[-1][0]:
+                stack.pop() 
+
+            if stack and nums[idx] > stack[-1][1]:
+                return True
+
+            stack.append([nums[idx], min_val])
+            min_val = min(min_val, nums[idx])
+        return False
