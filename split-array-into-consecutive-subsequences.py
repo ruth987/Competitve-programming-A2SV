c0dfd86e684d9e57f48dc783f1122e479d7ba782
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-05-07 10:45:33 +0300

    added split-array-into-consecutive-subsequences.python

---
+++ split-array-into-consecutive-subsequences.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPossible(self, nums: List[int]) -> bool:
+        counter = collections.Counter(nums)
+        end = defaultdict(int)
+
+        for num in nums:
+            if counter[num]:
+                counter[num] -= 1
+                if end[num-1]:
+                    end[num] += 1
+                    end[num-1] -=1
+                # if it is a new sequence beginning
+                elif counter[num+1] and counter[num+2]:
+                    counter[num+1] -= 1
+                    counter[num+2] -= 1
+                    end[num+2] += 1
+                else:
+                    return False
+        return True
