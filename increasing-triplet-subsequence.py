c707dff084eae1331f04426de4e19da47dc0bf46
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-05-25 18:50:45 +0300

    added increasing-triplet-subsequence.python

--- increasing-triplet-subsequence.py
+++ increasing-triplet-subsequence.py
@@ -1,29 +1,11 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        """
-        what if we try to store the minimum sofar and
-        the maximum so far as we go backwards
-        then check if a smaller number before and larger number after that number
-        exists
-        """
-        #min sofar
-        n = len(nums)
-        min_sofar = [0]*n
-        minn = float('inf')
-        for idx in range(n):
-            if nums[idx] < minn:
-                minn = nums[idx]
-            min_sofar[idx] = minn
-        # max sofar
-        max_sofar = [0]*n
-        maxx = float('-inf')
-        for idx in range(n-1, -1, -1):
-            if nums[idx] > maxx:
-                maxx = nums[idx] 
-            max_sofar[idx] = maxx
-        
-        # check if such number exists
-        for i in range(n):
-            if min_sofar[i] < nums[i] < max_sofar[i]:
+        min1 = min2 = float("inf")
+        for i, n in enumerate(nums):
+            if min1 < min2 < n:
                 return True
+            elif n < min1:
+                min1 = n
+            elif min1 < n < min2:
+                min2 = n
         return False
