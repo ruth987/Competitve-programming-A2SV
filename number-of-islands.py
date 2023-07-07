d6e72e3f65302d9908f7f6440735916fa79c6c80
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-07-07 17:17:26 +0300

    added number-of-islands.python

--- number-of-islands.py
+++ number-of-islands.py
@@ -1,32 +1,37 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         """
-        an island is ones connected 4 directionally
-        we can do a dfs: 
-        by iterating through the grid we can call our dfs function in each ones
-        that arenot already visited by the island created with the other one 
-        then we can count the number of islands when ever we call the function
+        try it with bfs
 
         """
         rows = len(grid)
         cols = len(grid[0])
         visited = set()
-
-        def dfs(r,c):
-            if r<0 or r==rows or c<0 or c==cols or (r,c) in visited or grid[r][c]=="0":
-                return 
-
-            visited.add((r,c))
-            dfs(r+1, c)
-            dfs(r, c+1)
-            dfs(r-1, c)
-            dfs(r, c-1)
-
         count = 0
+
+        def isValid(r,c):
+            if r<0 or r==rows or c < 0 or c == cols:
+                return False
+            return True
+
+        def bfs(r,c):
+            qu = deque()
+            qu.append((r,c))
+
+            while qu:
+                length = len(qu)
+                for i in range(length):
+                    row,col = qu.popleft()
+                    if not isValid(row,col) or (row,col) in visited  or grid[row][col]=="0": continue
+                    visited.add((row,col))
+                    qu.append((row+1, col))
+                    qu.append((row-1, col))
+                    qu.append((row, col+1))
+                    qu.append((row, col-1))
+
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1" and (r,c) not in visited:
-                    dfs(r, c)
+                    bfs(r,c)
                     count += 1
-
         return count
