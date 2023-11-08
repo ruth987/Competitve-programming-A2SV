5ed263c7a52f91101d3b6b12f67887c7ca81964b
ruth987 <99682266+ruth987@users.noreply.github.com>
2023-11-08 15:32:42 +0300

    Added solution for Longest Increasing Subsequence

---
+++ leet-code-solutions/longest-increasing-subsequence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        sub = []
+        for x in nums:
+            if len(sub) == 0 or sub[-1] < x:
+                sub.append(x)
+            else:
+                idx = bisect_left(sub, x)
+                sub[idx] = x
+
+        return len(sub)
